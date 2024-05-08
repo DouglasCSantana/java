3ede4278db6592704e5d88f71a5452a1b65a89de
Douglas Cosya Santana <douglas.costa.stna@gmail.com>
2024-05-08 18:48:07 -0300

    sdads

--- src/main/java/sadsda/EstruturaDeDecisao.java
+++ src/main/java/sadsda/EstruturaDeDecisao.java
@@ -2,11 +2,12 @@ package sadsda;
 
 public class EstruturaDeDecisao {
     public static void main(String[] args) {
-
-        if (true){
-            System.out.println("verdadeiro");
+        float numero = Math.round(5.5);
+        if (numero % 2 ==0){
+            System.out.println("par");
         }else{
-            System.out.println("falso");
+            System.out.println("impar");
         }
+
     }
 }
