375c0a806cc3325b304657cb089659be742ea7fa
Douglas Cosya Santana <douglas.costa.stna@gmail.com>
2024-05-07 21:50:58 -0300

    sdads

---
+++ src/main/java/sadsda/EstruturaDeDecisao.java
@@ -0,0 +1,12 @@
+package sadsda;
+
+public class EstruturaDeDecisao {
+    public static void main(String[] args) {
+
+        if (true){
+            System.out.println("verdadeiro");
+        }else{
+            System.out.println("falso");
+        }
+    }
+}
